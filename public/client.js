bdeda34f538ec80ecadacc0b4b150098a9b48a44
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-07-20 18:43:13 -0500

    styling updates

--- public/client.js
+++ public/client.js
@@ -4,11 +4,17 @@ $(document).ready(function() {
     $('form').submit(function() {
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
+        updateScroll();
         return false;
     });
     socket.on('chat message', function(msg) {
         $('#messages').append($('<li>').text(msg));
+        updateScroll();
     });
 
+    function updateScroll() {
+        var element = document.getElementById("chat");
+        element.scrollTop = element.scrollHeight;
+    }
 
 });
